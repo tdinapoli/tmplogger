e5eee5479fce211a02a93b9533b30b004a3266ea
tdinapoli <tomas.dina98@gmail.com>
2025-03-06 18:35:43 +0100

    add plot of the temperature

--- src/tmplogger/main.py
+++ src/tmplogger/main.py
@@ -5,8 +5,14 @@ import tkinter as tk
 from tkinter import ttk
 import sys
 import pathlib
+import time
 
 from logging_config import get_temperature_logger, init_logging_config, init_logging_dirs
+import matplotlib.pyplot as plt
+from matplotlib.backend_bases import key_press_handler
+from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg,
+                                               NavigationToolbar2Tk)
+from matplotlib.figure import Figure
 from mercury_itc import MercuryiTC
 import pyvisa
 import sv_ttk
@@ -48,21 +54,26 @@ class App:
 
         self.root.protocol("WM_DELETE_WINDOW", self.close_window)
 
+        self.temperatures = []
+        self.times = []
+        self.time_start = time.time()
+        self.init_plot()
+
     def setup_ui(self):
         # Port selection
         self.port_label = ttk.Label(self.root, text="Select Port:")
-        self.port_label.grid(row=0, column=0, padx=10, pady=10)
+        self.port_label.grid(row=0, column=1, padx=10, pady=10)
 
         self.port_combobox = ttk.Combobox(self.root, values=self.get_available_ports(), state="readonly")
-        self.port_combobox.grid(row=0, column=1, padx=10, pady=10)
+        self.port_combobox.grid(row=0, column=2, padx=10, pady=10)
 
         # Logging button
         self.toggle_button = ttk.Button(self.root, text="Toggle Logging", command=self.toggle_logging)
-        self.toggle_button.grid(row=0, column=2, padx=10, pady=10)
+        self.toggle_button.grid(row=0, column=3, padx=10, pady=10)
 
         # Status label
         self.status_label = ttk.Label(self.root, text="Status: Stopped", foreground="red")
-        self.status_label.grid(row=2, column=0, columnspan=3, pady=10)
+        self.status_label.grid(row=0, column=4, columnspan=1, padx=10)
 
         # Configure grid resizing
         self.root.grid_rowconfigure(1, weight=1)
@@ -101,10 +112,44 @@ class App:
     def log_tmp(self, query_fn):
         tmp = query_fn()
         self.logger.info(f"{tmp}")
+        return tmp
+
+    def init_plot(self):
+        self.fig = Figure((10, 5), dpi=100)
+        self.ax = self.fig.add_subplot()
+        self.ax.grid()
+        self.ax.set_xlabel('Time [s]', fontsize=20)
+        self.ax.set_ylabel('Temperature [K]', fontsize=20)
+
+        self.canvas = FigureCanvasTkAgg(self.fig, master=self.root)
+        self.canvas.draw()
+
+        self.line, = self.ax.plot([0], [0], ".-", linewidth=2, color="C0")
+
+        #toolbar = NavigationToolbar2Tk(self.canvas, self.root, pack_toolbar=False)
+        #toolbar.update()
+
+        self.canvas.mpl_connect(
+            "key_press_event", lambda event: print(f"you pressed {event.key}"))
+        self.canvas.mpl_connect("key_press_event", key_press_handler)
+
+        #toolbar.pack(side=tk.BOTTOM, fill=tk.X)
+        self.canvas.get_tk_widget().grid(row=0, column=0, padx=10, pady=10)
+
+
+    def update_plot(self):
+        self.line.set_data(self.times, self.temperatures)
+        self.ax.set_xlim([-1, self.times[-1] + 1])
+        self.ax.set_ylim([min(self.temperatures) - 1, max(self.temperatures) + 1])
+        self.canvas.draw()
+        print(self.times, self.temperatures)
 
 
     def run(self, query_fn):
-        self.log_tmp(query_fn)
+        tmp = self.log_tmp(query_fn)
+        self.times.append(time.time() - self.time_start)
+        self.temperatures.append(float(tmp))
+        self.update_plot()
         self.timer = threading.Timer(5.0, lambda: self.run(query_fn))
         self.timer.start()
         self.is_logging = True
@@ -131,7 +176,7 @@ def main():
     init_logging_config(ROOT)
     root_tk = tk.Tk()
     app = App(root_tk, path=ROOT)
-    sv_ttk.set_theme('dark')
+    sv_ttk.set_theme('light')
     root_tk.mainloop()
 
 if __name__ == "__main__":
